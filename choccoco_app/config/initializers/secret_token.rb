6c2b6fe592c288ee5e66591fc616983d6627ccba
yutokyokutyo <k.yuto.cre@gmail.com>
2014-12-07 20:06:29 +0900

    Ruby on Rails を導入しました

---
+++ choccoco_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Choccoco::Application.config.secret_key_base = '870c88e29d0e36b9d4306c0bc9aa6e6c4e54b31c6e53b924d33080d8d25dcfdfab4374933a1d320f6bad672bf796cc7ef2d292005ed9f30d5426d413d2912f61'
